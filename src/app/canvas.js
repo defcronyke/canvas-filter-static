9f7616c45907b693bbb8cb61ef7dea59fe60e089
Jeremy Carter <jeremy@jccss.ca>
2017-12-31 21:13:45 -0500

    now you can scale the images

--- src/app/canvas.js
+++ src/app/canvas.js
@@ -12,13 +12,47 @@ export default class Canvas extends Component {
     this.img = null;
     this.filter = null;
 
+    this.state = {
+      shrinkImage: true,
+      shrinkImageMultiple: 4
+    };
+
+    this.updateFilters = () => {
+      this.filter.mod45Percent.update(this.canvas, this.ctx);  // NOTE: Add a call to update for each filter here
+    };
+
+    this.handleShrinkImage = () => {
+      this.setState({
+        shrinkImage: !this.state.shrinkImage
+      }, () => {
+        this.canvas.width = this.state.shrinkImage ? this.img.width / this.state.shrinkImageMultiple : this.img.width;
+        this.canvas.height = this.state.shrinkImage ? this.img.height / this.state.shrinkImageMultiple : this.img.height;
+        this.ctx.drawImage(this.img, 0, 0, this.canvas.width, this.canvas.height);
+        this.updateFilters();
+      });
+    };
+
+    this.handleShrinkImageMultiple = e => {
+      const num = e.target.value;
+      if (!this.state.shrinkImage) {
+        return;
+      }
+      this.setState({
+        shrinkImageMultiple: num
+      }, () => {
+        this.canvas.width = this.state.shrinkImage ? this.img.width / this.state.shrinkImageMultiple : this.img.width;
+        this.canvas.height = this.state.shrinkImage ? this.img.height / this.state.shrinkImageMultiple : this.img.height;
+        this.ctx.drawImage(this.img, 0, 0, this.canvas.width, this.canvas.height);
+        this.updateFilters();
+      });
+    };
+
     this.imageLoaded = () => {
       console.log('image loaded');
-      this.canvas.width = this.img.width;
-      this.canvas.height = this.img.height;
-      this.ctx.drawImage(this.img, 0, 0);
-
-      this.filter.mod45Percent.update(this.canvas, this.ctx);  // NOTE: Add a call to update for each filter here
+      this.canvas.width = this.state.shrinkImage ? this.img.width / this.state.shrinkImageMultiple : this.img.width;
+      this.canvas.height = this.state.shrinkImage ? this.img.height / this.state.shrinkImageMultiple : this.img.height;
+      this.ctx.drawImage(this.img, 0, 0, this.canvas.width, this.canvas.height);
+      this.updateFilters();
     };
 
     this.handleLoadFileReader = e => {
@@ -44,6 +78,7 @@ export default class Canvas extends Component {
     this.imageFileInput.addEventListener('change', this.handleLoadImageFile);
     this.filter = new Filter(this.canvas, this.ctx);
 
+    document.getElementById('normal').addEventListener('click', this.imageLoaded);
     document.getElementById('mod45Percent').addEventListener('click', this.filter.mod45Percent.handleApply);  // NOTE: Add new filter event handlers here
   }
 
@@ -55,12 +90,28 @@ export default class Canvas extends Component {
             type="file"
             id="imageFileInput"
             />
+          <span>
+            Shrink Image:
+            <input
+              type="checkbox"
+              onChange={this.handleShrinkImage}
+              defaultChecked={this.state.shrinkImage}
+              value={this.state.shrinkImage}
+              />
+            <span> Shrink Multiple: </span>
+            <input
+              type="number"
+              onChange={this.handleShrinkImageMultiple}
+              value={this.state.shrinkImageMultiple}
+              />
+          </span>
         </div>
         <br/>
         <div>
           <div>
             <span>Filters: </span>
 
+            <button id="normal">Normal</button>
             <button id="mod45Percent">Mod45Percent</button> {/* NOTE: Add new filter buttons here */}
 
           </div>
